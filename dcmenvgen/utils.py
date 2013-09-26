a9967f8ab1bd02a1b810d5c0090db5d2fd245b1b
Eric Zarowny <ezarowny@gmail.com>
2013-09-26 15:44:10 -0500

    Some work on populate mode completed. DICOM directories and files with patient information are being created.

--- dcmenvgen/utils.py
+++ dcmenvgen/utils.py
@@ -1,4 +1,6 @@
 import datetime
+import dicom
+import os
 import random
 import string
 
@@ -10,3 +12,33 @@ def random_string(size, chars=string.ascii_letters + string.digits):
 def random_date_between(start, end):
     return start + datetime.timedelta(
         seconds=random.randint(0, int((end - start).total_seconds())))
+
+
+def create_dicom_file(patient, study, series, image, image_path):
+    original_path = os.path.join(os.path.dirname(dicom.__file__), 'testfiles/CT_small.dcm')
+    original = dicom.read_file(original_path)
+    
+    # create meta header
+    file_meta = dicom.dataset.Dataset()
+    file_meta.MediaStorageSOPClassUID = image.sop_class_uid
+    file_meta.MediaStorageSOPInstanceUID = image.sop_instance_uid
+    file_meta.ImplementationClassUID = dicom.UID.pydicom_UIDs.keys()[0]
+
+    # create DICOM dataset
+    ds = dicom.dataset.FileDataset(image_path, {}, file_meta=file_meta, preamble="\0" * 128)
+
+    # add patient information
+    ds.PatientID = patient.id
+    ds.PatientSex = patient.sex
+    ds.PatientName = '{0}^{1}'.format(patient.last_name, patient.first_name)
+    ds.PatientBirthDate = patient.birth_date.date().strftime('%Y%m%d')
+
+    # add study information
+
+    # add series information
+
+    # add image information
+
+    # save file with dicom extension
+    print ds
+    ds.save_as(image_path + '.dcm')
