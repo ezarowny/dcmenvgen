19d42a0ca33b67c6d5b41d55ce3999cbcbd0984f
Eric Zarowny <ezarowny@gmail.com>
2013-10-21 11:10:53 -0500

    Unit test skeleton.

---
+++ tests/dcmenvgen_tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class TestClass(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # helper functions
+
+    def function(self):
+        pass
+
+    # testing functions
+
+    def test_name(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
